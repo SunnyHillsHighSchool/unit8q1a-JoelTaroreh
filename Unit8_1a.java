1fbc3fd6c538445a920a5ee832716594d73c8231
Joel Taroreh <>
2021-04-29 05:47:00 +0000

    complete

--- Unit8_1a.java
+++ Unit8_1a.java
@@ -2,6 +2,11 @@ public class Unit8_1a
 {
   public int [] [] modifyColumns (int [] [] arr)
  { 
+   for(int i = 1; i < arr[0].length; i+=2) {
+     for(int j = 0; j < arr.length; j++) {
+       arr[j][i] = arr[j][i-1];
+     }
+   }
 return arr;
       }
     }
